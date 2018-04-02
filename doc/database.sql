92dc6018f59fdec047f7d52200645dc704d42d38
helena hass <helena.hass@khk.ee>
2018-04-02 12:50:40 +0300

    Updated database dump

--- doc/database.sql
+++ doc/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2018 at 10:05 AM
+-- Generation Time: Apr 02, 2018 at 11:49 AM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
