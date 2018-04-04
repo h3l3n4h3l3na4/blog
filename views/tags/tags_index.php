28f7cb45b5efaf47dade4baa3546ca7716b074df
helena hass <helena.hass@khk.ee>
2018-04-04 16:01:20 +0300

    Add tags support

--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -1,5 +1,7 @@
 <ul class="list-group">
     <? foreach($tags as $tag): ?>
-        <a href="#"><li class="list-group-item">php<?=$tag['tag_name']; ?></li></a>
+    <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><li class="list-group-item"><?=$tag['tag_name']?></li></a>
     <? endforeach ?>
 </ul>
+
+tagsindex
---
+++ views/tags/tags_view.php
@@ -0,0 +1,18 @@
+<div class="span8">
+    <?foreach($posts as $post):?>
+        <h3><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?><?= $post['post_subject']; ?></a></h3>
+        <p><?= $post['post_text']; ?></p>
+
+        <div>
+
+            <span class="badge badge-success">Posted <?= $post['post_created']; ?></span><div class="pull-right">
+
+                <? foreach($tags[$post['post_id']] as $tag): ?>
+                    <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?><span class="label label-primary"><?=$tag['tag_name']?></span></a>
+                <?endforeach?></div>
+        </div>
+
+    <?endforeach?>
+</div>
+
+tagsview
