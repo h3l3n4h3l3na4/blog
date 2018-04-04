c0500934ba77d59924f380d4e2cbee9e2277eefe
helena hass <helena.hass@khk.ee>
2018-04-04 16:00:10 +0300

    Adding comments function

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,12 +1,27 @@
 <div class="span8">
     <?php foreach($posts as $post):?>
-        <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post['post_subject'];?></a></h1>
-        <p><?=$post['post_text'];?></p>
+        <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><h1><?=$post['post_subject']?></h1></a>
+        <p><?=$post['post_text']?></p>
+
         <div>
-                <span class="badge badge-success">Posted <?=$post['post_created']; ?></span>
-                <div class="pull-right">
-                    <span class="label"><?=$post['user_id'];?></span>
-                </div>
+            <span class="badge badge-success">Posted <?=$post['post_created']?></span>
+        </div>
+
+
+
+        <div>
+        <?php foreach ($tags[$post['post_id']] as $tag):?>
+            <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"></a>
+
+            <div>
+                <? foreach ($tags[$post['post_id']] as $tag): ?>
+                <a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><span class="label label-primary"><?=$tag['tag_name']?></span></a>
+                <?endforeach?>
             </div>
+
+        <?php endforeach?>
+        </div>
     <?php endforeach?>
 </div>
+
+postindex
--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -1,4 +1,4 @@
-<div class="controller">
+<div class="container">
     <div class="well">
         <div class="media">
             <div class="media-body">
@@ -6,14 +6,14 @@
                 <p class="text-right"><?=$post['name']?></p>
                 <p><?=$post['post_text']?></p>
                 <ul class="list-inline list-unstyled">
-                    <li><span><i class="glyphicon glyphicon-calendar"></i><?php echo $post['post_created']?></span></li>
+                    <li><span><i class="glyphicon glyphicon-calendar"></i><?=$post['post_created']?></span></li>
                     <li>|</li>
                     <?php foreach ($tags as $tag):?>
-                        <a href="#">
-                            <span class="label label-info">
+                        <li><a href="<?=BASE_URL?>tags/view/<?=$tag['tag_id']?>
+                        <span class="label label-info">
                             <?=$tag['tag_name']?>
-                            </span>
-                        </a>&nbsp;</li>
+                        </span>
+                            </a>&nbsp;</li>
                     <?php endforeach?>
                     <li>|</li>
                     <span><i class="glyphicon glyphicon-comment"></i> ? comments</span>
@@ -22,3 +22,25 @@
         </div>
     </div>
 </div>
+
+
+
+<div class="container">
+    <div class="row">
+        <div class="media comment-box">
+            <div class="media-body">
+
+                <?php foreach($comments as $comment): ?>
+                    <h4 class="media-heading"></h4>
+                    <p><?php echo $comment['comment_text']?></p>
+                    <span class="badge badge-success">Commented on <?php echo $comment['comment_created']?></span> <span class="badge badge-success">Author: <?php echo $comment['comment_author']?></span>
+
+                    <br>
+                <?php endforeach?>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+postsview
